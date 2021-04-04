99ff005ddfc92647ab6245fbe9dff8dd1723bff2
Ryan Kemmer <ryankemmer77@gmail.com>
2021-04-03 19:12:35 -0700

    updates

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,22 @@ var express = require('express');
 var dbService = require('../dbService');
 var router = express.Router();
 const bcrypt = require('bcryptjs');
+var axios = require('axios');
 
 /* Mongo DB options */
 var url = 'mongodb://localhost:27017/';
 var datab = 'app-prototype'
 const saltRounds = 10;
+
+function makeid(length) {
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
 
 // welcome page
 router.get('/welcome', async (req,res) => {
@@ -71,7 +82,7 @@ router.post('/login', async (req, res) => {
   const message = await db.signIn(email,req.body.password);
 
   if (message === null){
-    
+
     res.redirect('index')
 
   } else{
@@ -87,13 +98,48 @@ router.post('/login', async (req, res) => {
 router.post('/createmap', async (req, res) => {
   
   let group = Object.keys(req.body);
-  //group = JSON.parse(group)
+  group = JSON.parse(group)
 
-  console.log(group)
   email = group[0]
   lat = group[1]
   lon = group[2]
   coordinates = group[3]
+  console.log(coordinates)
+
+  //create new polygon
+  var api_key = '92746ad71c2ebfbb81d42d2ed6d152ff';
+  var id = makeid(7)
+  console.log(id)
+
+  json = {
+
+    "name": id,
+    "geo_json" : {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [
+                [
+                  coordinates[0],coordinates[1],coordinates[2],coordinates[3],coordinates[0]
+                ]
+            ]
+        }
+    }
+}
+
+  const resp = await axios.post(`http://api.agromonitoring.com/agro/1.0/polygons?appid=${api_key}`, json, {
+      headers: {'Content-Type': 'application/json'}
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+
+  console.log(resp)
+  polyID = group
 
   const db = dbService.getDbServiceInstance();
   const message = await db.addMap(email,lat,lon,coordinates);
