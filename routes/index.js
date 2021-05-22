e1c1ae33dbb289595e5919943894ea885eceaaa0
joshstovall <joshstovall@gmail.com>
2021-05-22 02:09:57 -0700

    update
    can change date range of growth video
    added d3 graph to weather page

--- routes/index.js
+++ routes/index.js
@@ -210,6 +210,98 @@ router.get('/getVideoURL', async (req, res) => {
 
 });
 
+
+// NEW VIDEO
+router.post('/getVideoURL', async (req, res) => {
+	
+	
+	
+	console.log('start date: '+req.body.start);
+		console.log('end date: '+req.body.end);
+	
+	
+	
+  var today = new Date(req.body.end);
+
+  var dd = ('0' + today.getDate()).slice(-2)
+  var mm = ('0' + (today.getMonth()+1)).slice(-2)
+  var yyyy = today.getFullYear();
+
+  var currDateStr = yyyy + '-' + mm + '-' + dd
+
+  var yearAgo = new Date(req.body.start);
+//  yearAgo.setFullYear( yearAgo.getFullYear() - 1 );
+
+  dd = ('0' + yearAgo.getDate()).slice(-2)
+  mm = ('0' + (yearAgo.getMonth()+1)).slice(-2)
+  yyyy = yearAgo.getFullYear();
+
+  var currDateMinusYear = yyyy + '-' + mm + '-' + dd
+
+  const sessionToken = req.cookies.sessionToken;
+  console.log(sessionToken)
+  const user = getUserBySession(sessionToken)
+  const db = dbService.getDbServiceInstance();
+  const userObj = await db.getUser(email);
+  var userbbox = userObj.bbox
+
+  var points = userObj.points
+
+  var aoi = ee.Geometry.Polygon(
+    [[points[0],points[1],points[2],points[3]]], null,
+    false);
+
+  var cloudMask = function(img) {
+      var scored = ee.Algorithms.Landsat.simpleCloudScore(img);
+      var mask = scored.select(['cloud']).lte(20);
+      return img.updateMask(mask);
+  };
+
+  var imgCol = ee.ImageCollection('LANDSAT/LC08/C01/T1_RT')
+    .filterBounds(ee.Geometry.BBox(userbbox[0],userbbox[1],userbbox[2],userbbox[3]))
+    .filterDate(currDateMinusYear, currDateStr)
+    .sort('system:time_start', true)
+    .limit(500)
+
+  var imgMask = imgCol.map(cloudMask)
+
+  var lsCompList = imgMask.map(function(img) {
+      // Get the list of images belonging to the given year.
+      return img
+        // Apply cloud mask.
+        .visualize({bands:['B4', 'B3', 'B2'], gamma: 1.5})
+        .resample('bicubic')
+        // Set composite year as an image property.
+    });
+
+
+  var lsCompCol = ee.ImageCollection(lsCompList);
+
+  // Define arguments for animation function parameters.
+  var gifParams = {
+    'region': aoi,
+    'dimensions': 600,
+    'crs': 'EPSG:3857',
+    'framesPerSecond': 4
+  };
+
+  console.log(lsCompCol.getVideoThumbURL(gifParams));
+  var url = lsCompCol.getVideoThumbURL(gifParams)
+  res.send({url})
+	
+	/*
+	
+	
+	console.log("someone came in here");
+	console.log("someone came in here");
+	*/
+	
+	
+   // console.log(req.body.user);
+   // console.log(req.body.password);
+});
+
+
 //NDVI MAP 
 //NOTE: Does not work up close
 router.get('/getVideoURL2', async (req, res) => {
